862faf5ba7825b88f8a34392b6f29b9dff3120c2
Nili Scharf <nilischarf@gmail.com>
2025-01-26 17:25:07 +0200

    complete sushi saga

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import SushiContainer from "./SushiContainer";
 import Table from "./Table";
 
 const API = "http://localhost:3001/sushis";
 
+const STARTING_BUDGET = 50;
+
 function App() {
+  const [eatenSushiIds, setEatenSushiIds] = useState([]);
+  const [budget, setBudget] = useState(STARTING_BUDGET);
+
+  const handleEatSushi = (sushi) => {
+    if (budget >= sushi.price) {
+      setEatenSushiIds([...eatenSushiIds, sushi.id]);
+      setBudget(budget - sushi.price);
+    } else {
+      alert("You don't have enough money for this sushi!");
+    }
+  };
+
   return (
     <div className="app">
-      <SushiContainer />
-      <Table />
+      <SushiContainer
+        onEatSushi={handleEatSushi}
+        eatenSushiIds={eatenSushiIds}
+      />
+      <Table plates={eatenSushiIds} budget={budget} />
     </div>
   );
 }
--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,11 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Sushi from "./Sushi";
 import MoreButton from "./MoreButton";
 
-function SushiContainer(props) {
+const API = "http://localhost:3001/sushis";
+
+function SushiContainer({ onEatSushi, eatenSushiIds }) {
+  const [sushis, setSushis] = useState([]);
+  const [index, setIndex] = useState(0);
+  
+  useEffect(() => {
+    fetch(API)
+      .then((response) => response.json())
+      .then((data) => setSushis(data));
+  }, []);
+
+  const displaySushis = sushis.slice(index, index + 4);
+
+  const handleMoreSushi = () => {
+    setIndex((prevIndex) => prevIndex + 4);
+  };
+
   return (
     <div className="belt">
-      {/* Render Sushi components here! */}
-      <MoreButton />
+      {displaySushis.map((sushi) => (
+        <Sushi
+          key={sushi.id}
+          sushi={sushi}
+          onEatSushi={onEatSushi}
+          isEaten={eatenSushiIds.includes(sushi.id)}
+        />
+      ))}
+      <MoreButton onClick={handleMoreSushi} />
     </div>
   );
 }
